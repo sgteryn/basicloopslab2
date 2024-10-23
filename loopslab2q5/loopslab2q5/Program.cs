a508b34446b34bff3944b07a178e49054609a1be
Teryn Milliner <107887744+sgteryn@users.noreply.github.com>
2024-10-23 02:09:45 -0400

    Q5 commit
    Q5 commit

---
+++ loopslab2q5/loopslab2q5/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int keyPadPin = 13579;
+
+bool doorLocked = true;
+
+int numberOfTries = 5;
+
+int i = 0;
+
+do
+{
+    Console.WriteLine("Enter the Key Code: ");
+    int keyEntered = int.Parse(Console.ReadLine());
+
+    i++;
+
+    if (keyEntered == keyPadPin)
+    {
+        Console.WriteLine("Welcome, you may enter!");
+        doorLocked = false;
+        break;
+    }
+    else if (doorLocked == true && i == numberOfTries)
+    {
+        Console.WriteLine("Too Many incorrect attempts");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+
+} while (doorLocked == true && i < numberOfTries);
+
